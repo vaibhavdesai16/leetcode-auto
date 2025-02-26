431b264d597c46a5f4b86c5612d2bd14e44a73a6
Vaibhav Desai <vaibhavdesai16@gmail.com>
2025-02-26 11:29:10 +0530

    Time: 19 ms (9.81%) | Memory: 48.8 MB (33.04%) - LeetSync

---
+++ 554-brick-wall/brick-wall.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+        HashMap<Integer,Integer> frq = new HashMap<>();
+
+        for(int i=0; i<wall.size(); i++){
+            List<Integer> column = wall.get(i);
+            for(int j=0; j<column.size()-1; j++){
+
+                if(j==0){
+                    column.set(j,column.get(j));
+                }
+                else{
+                    column.set(j,column.get(j-1)+column.get(j));
+                }
+
+                int num = column.get(j);
+                frq.put(num, frq.getOrDefault(num, 0) + 1);
+            }
+        }
+        int max = 0;
+        for (Integer key : frq.keySet()) {
+            int frequncy = frq.get(key);
+            max = Math.max(max, frequncy);
+        }
+        System.out.println(max);
+        return wall.size() - max;
+    }
+}
