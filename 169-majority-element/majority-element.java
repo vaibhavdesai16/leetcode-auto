22bff14b27505b2705d74b3d570a31708a843e74
Vaibhav Desai <vaibhavdesai16@gmail.com>
2025-02-27 07:22:32 +0530

    Time: 7 ms (38.70%) | Memory: 53.1 MB (58.79%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -2,10 +2,7 @@ class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
         int count = 0;
-
         float half = nums.length/2.0f;
-        //System.out.println(half);
-
         for(int i=0; i<nums.length; i++){
             
             count++;
@@ -13,22 +10,15 @@ class Solution {
                 if(count > half){
                     return nums[i];
                 }
-                nums[i] = count;
             }
 
             else if(nums[i] < nums[i+1]){
                 if(count > half){
                     return nums[i];
                 }
-                nums[i] = count;
                 count = 0;
             }
         }
-
-        // for(int n : nums){
-        //     System.out.println(n);
-        // }
-
         return -1;
     }
 }
