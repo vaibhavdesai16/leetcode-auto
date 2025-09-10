0609f1a28ff66ee3709e8687ff56017ca14fae3c
Vaibhav Desai <vaibhavdesai16@gmail.com>
2025-09-10 19:18:35 +0530

    Time: 9 ms (43.81%) | Memory: 46.8 MB (26.41%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -4,19 +4,16 @@ class Solution {
         int merge = 0;
         int len = intervals.length;
 
-        for(int i = 0; i < intervals.length - 1; i++){
+        for(int i = 0; i < len - 1; i++){
             
-            int[] curr = intervals[i];
-            int[] next = intervals[i+1];
-
             // check for overlap
-            if(next[0] <= curr[1]){
+            if(intervals[i+1][0] <= intervals[i][1]){
                 // merge
-                next[0] = curr[0];
-                next[1] = next[1] > curr[1] ? next[1] : curr[1];
+                intervals[i+1][0] = intervals[i][0];
+                intervals[i+1][1] = intervals[i+1][1] > intervals[i][1] ? intervals[i+1][1] : intervals[i][1];
                 // exclude curr
-                curr[0] = -1;
-                curr[1] = -1;
+                intervals[i][0] = -1;
+                intervals[i][1] = -1;
                 merge++;
             }
             
@@ -25,10 +22,7 @@ class Solution {
         int[][] ans = new int[len - merge][2];
         int pointer = 0;
 
-        for(int i = 0; i < intervals.length; i++){
-
-            // System.out.println("first " + intervals[i][0] + " second " + intervals[i][1]);
-            // System.out.println("merged " + merge);
+        for(int i = 0; i < len; i++){
 
             if(intervals[i][0] != -1){
                 ans[pointer][0] = intervals[i][0];
