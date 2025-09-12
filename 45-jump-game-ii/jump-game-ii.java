b993924804e2b3eb833a5d8462ef31e048ece7fd
Vaibhav Desai <vaibhavdesai16@gmail.com>
2025-09-12 21:49:08 +0530

    Time: 45 ms (11.77%) | Memory: 45.7 MB (13.68%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,28 +1,28 @@
 class Solution {
     public int jump(int[] nums) {
+
         int[] dp = new int[nums.length];
+
         dp[0] = 0;
 
-        for(int i = 1; i < dp.length; i++){
-            dp[i] = 9999;
-        }
-
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i< nums.length; i++){
 
             for(int j = 1; j <= nums[i]; j++){
-                
                 int idx = i + j;
+                if(idx < nums.length){
 
-                if(idx < dp.length){
-                    dp[idx] = Math.min(dp[idx], dp[i] + 1);
+                    if(dp[idx] == 0){
+                        dp[idx] = dp[i] + 1;
+                    }
+                    else{
+                        int jupms = dp[i] + 1;
+                        dp[idx] = Math.min(dp[idx], jupms);
+                    }
                 }
             }
+
         }
 
-        // for(int i = 0; i < dp.length; i++){
-        //     System.out.println("i = " + i + " val " + dp[i]);
-        // }
-
-        return dp[nums.length -1];
+        return dp[nums.length-1];
     }
 }
